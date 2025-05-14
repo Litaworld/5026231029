5dd82e3f351ee646489fc50e27f574a0c776a8cf
litaworld <5026231029@student.its.ac.id>
2025-05-14 12:22:48 +0700

    pertemuan 11

---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Link;
+//import java.io;
+
+
+//system.out.println();
+Route::get('/', function () { // Route::get('published url', 'controller[simple html, view, function di ...]')
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+	return "<h2>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h2>";
+});
+
+Route::get('blog', function () {
+	return view('blog');
+});
+
+Route::get('hello', [Link::class, 'helloworld']);
+
+Route::get('pertama', function () {
+	return view('pertama');
+});
+
+Route::get('bootsrapt1', function () {
+	return view('bootsrapt1');
+});
+
+Route::get('eminalinktree', function () {
+	return view('eminalinktree');
+});
+
+Route::get('js1', function () {
+	return view('js1');
+});
+
+Route::get('js2', function () {
+	return view('js2');
+});
+
+Route::get('layoutexercise029', function () {
+	return view('layoutexercise029');
+});
+
+Route::get('template1', function () {
+	return view('template1');
+});
+
+Route::get('test', function () {
+	return view('test');
+});
