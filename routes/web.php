ac34f9df8eef2e0e815804a1f276672a10266030
litaworld <5026231029@student.its.ac.id>
2025-06-04 12:17:04 +0700

    Pertemuan 14

--- routes/web.php
+++ routes/web.php
@@ -82,7 +82,8 @@ Route::get('/blog/kontak', [blogcontroller::class, 'kontak']);
 //route CRUD
 Route::get('/pegawai', [PegawaiController::class, 'index']);
 Route::get('/pegawai/tambah', [PegawaiController::class, 'tambah']);
-Route::get('/pegawai/store', [PegawaiController::class, 'store']);
+Route::post('/pegawai/store', [PegawaiController::class, 'store']);
 Route::get('/pegawai/edit/{id}', [PegawaiController::class, 'edit']);
 Route::post('/pegawai/update', [PegawaiController::class, 'update']);
 Route::get('/pegawai/hapus/{id}', [PegawaiController::class, 'hapus']);
+Route::get('/pegawai/cari', [PegawaiController::class, 'cari']);
